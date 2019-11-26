03f7c418981ca648df739b6b52206717f6b12fce
xcm1115 <997749670@qq.com>
2019-11-27 01:02:59 +0800

    login and reg

--- tennisworld-test/login.php
+++ tennisworld-test/login.php
@@ -1,20 +1,33 @@
 <?php
-$link = mysqli_connect('cdb-7c1trg88.bj.tencentcdb.com:10203', 'root', 'tennisworld123') 
-		or die ('连接失败 : ' . mysqli_error());
+$link = mysqli_connect('cdb-7c1trg88.bj.tencentcdb.com:10203', 'root', 'tennisworld123') or die('连接失败 : ' . mysqli_error());
 
-mysqli_select_db($link,'tennisworld');
-mysqli_set_charset($link,'utf8');
+mysqli_select_db($link, 'tennisworld');
+mysqli_set_charset($link, 'utf8');
 
-$username=$_POST['username'];
-$password=$_POST['password'];
- 
-$sql="select * from reg_info where username='$username' AND password='$password'";
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$result=mysqli_query($link,$sql);
-if($result->num_rows!=0){ 
-	echo '<html><head><Script Language="JavaScript">alert("登录成功！");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=main.html\">";
-}else{
-	echo '<html><head><Script Language="JavaScript">alert("账号或密码错误！");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=login.html\">";
+$sql = "select * from reg_info where username='$username' AND password='$password'";
+
+$result = mysqli_query($link, $sql);
+
+if ($result -> num_rows != 0) {
+	echo '<html>
+			<head>
+				<Script Language="JavaScript">
+					location.href="main.html";
+				</Script>
+			</head>
+		</html>';
+
+} else {
+	echo '<html>
+			<head>
+				<Script Language="JavaScript">
+					location.href="login.html";
+				</Script>
+			</head>
+		</html>';
 }
 
 mysqli_close($link);
--- tennisworld-test/reg.php
+++ tennisworld-test/reg.php
@@ -1,20 +1,31 @@
 <?php
+$link = mysqli_connect('cdb-7c1trg88.bj.tencentcdb.com:10203', 'root', 'tennisworld123') or die('连接失败 : ' . mysqli_error());
+mysqli_select_db($link, 'tennisworld');
+mysqli_set_charset($link, 'utf8');
 
-$link = mysqli_connect('cdb-7c1trg88.bj.tencentcdb.com:10203', 'root', 'tennisworld123') 
-		or die ('连接失败 : ' . mysqli_error());
-mysqli_select_db($link,'tennisworld');
-mysqli_set_charset($link,'utf8');
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$username=$_POST['username'];
-$password=$_POST['password'];
- 
-$sql="insert into reg_info values('$username','$password')";
+$sql = "insert into reg_info values('$username','$password')";
 
-$result=mysqli_query($link,$sql);
-if($result){
-	echo '<html><head><Script Language="JavaScript">alert("注册成功！");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=main.html\">";
-}else{
-	echo '<html><head><Script Language="JavaScript">alert("用户已存在！");</Script></head></html>' . "<meta http-equiv=\"refresh\" content=\"0;url=reg.html\">";
+$result = mysqli_query($link, $sql);
+if ($result) {
+	echo '<script src="js/sweetalert.min.js"></script>';
+	echo '<html>
+			<head>
+				<Script Language="JavaScript">
+					location.href="main.html";
+				</Script>
+			</head>
+		</html>';
+} else {
+	echo '<html>
+			<head>
+				<Script Language="JavaScript">
+					location.href="reg.html";
+				</Script>
+			</head>
+		</html>';
 }
 
 mysqli_close($link);
